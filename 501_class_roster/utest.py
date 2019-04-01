e82a1e7fbd25913e196d50ec0c904ce322277388
KWANG KIM <kkim@qcc.cuny.edu>
2019-04-01 18:34:48 -0400

    editing roster with e2e

--- 501_class_roster/utest.py
+++ 501_class_roster/utest.py
@@ -38,6 +38,7 @@ def ureport(scores):
     avg=round(sum(scores)/len(scores))
     feedback.set_grade(avg)
     feedback.set_global_result("success" if avg >=90  else "failed")
-    feedback.set_global_inginious.feedback("The list of scores of each question is "+str([scores])+".")
+    score_list="["+(",".join(scores)+"]"
+    feedback.set_global_inginious.feedback("The list of scores of each question is "+store_list+".")
 
         
